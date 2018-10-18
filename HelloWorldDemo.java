92d07cbeecccddec83288c317e6e44643f6c60f2
Swamy <smitha.swamy@nttdata.com>
2018-10-18 15:15:29 +0530

    added files

---
+++ HelloWorldDemo.java
@@ -0,0 +1,14 @@
+
+public class HelloWorldDemo {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Everyone");
+		System.out.println("Welcome to session!!");
+		
+		
+
+	}
+
+}
+
